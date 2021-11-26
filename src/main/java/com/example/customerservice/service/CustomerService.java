51b348c3d6be70f3ccefb45004fc2e0e8d61527a
mainguyen18 <mai1851998@gmail.com>
2021-11-26 08:05:22 +0700

    Mai commit retry

--- src/main/java/com/example/customerservice/service/CustomerService.java
+++ src/main/java/com/example/customerservice/service/CustomerService.java
@@ -3,6 +3,7 @@ package com.example.customerservice.service;
 
 import com.example.customerservice.entity.Customer;
 import com.example.customerservice.repository.CustomerRepository;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,7 @@ public class CustomerService {
     public Customer saveCustomer(Customer order) {
         return customerRepository.save(order);
     }
-
+    @Retry(name="basic")
     public Customer findCustomerById(long id) {
         return customerRepository.findById(id).get();
     }
